1f1be9754323476f9d12f51c70e23e6d84660091
Mukunda sri <mukundasrichilukuri@gmail.com>
2026-07-11 16:06:34 +0530

    First project commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Employee Management System API Running");
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
